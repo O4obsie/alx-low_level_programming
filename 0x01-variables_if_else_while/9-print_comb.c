776243b84baf1bee801bfd283b44adaff5757d76
o4obsie <kodikinya@gmail.com>
2023-03-17 14:44:15 -0700

    print numbers with ','

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always o
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		putchar(num1);
+		putchar(',';
+		putchar(' ')
+	}
+	putchar('\n');
+	return (0);
+}

e8447f480633d4a7f83407239dca4fb47be39fc6
o4obsie <kodikinya@gmail.com>
2023-03-17 07:30:16 -0700

    print letters using for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lett;
+
+	for( lett = "a"; a < z; lett++)
+		putchar(lett);
+		putchar("\n");
+	return (0);
+}

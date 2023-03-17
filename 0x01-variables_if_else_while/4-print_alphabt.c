4fa3da88fc38fb77f4679a26bf4b9447d72f6ba9
o4obsie <kodikinya@gmail.com>
2023-03-17 12:13:01 -0700

    print from a to z(exclude q and e) using putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z'; let++)
+	{
+		if(lett != 'q' || lett != 'e')
+		{
+			putchar(lett);
+		}
+	}
+	putchar('\n');
+}

699e5e8074ec8382a48b92d3f36ee6a78592988f
o4obsie <kodikinya@gmail.com>
2023-03-17 06:33:28 -0700

    simple if else statements

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if( n > 0)
+	{
+		printf("%n is positive\n", n);
+	}
+	else if( n == 0)
+	{
+		printf("%n is zero\n", n);
+	}
+	else
+	{
+		printf("%n is negative\n", n);
+	}
+	return (0);
+}

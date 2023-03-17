558992ad15a5201b30b7402a76566cb20a1b6017
o4obsie <kodikinya@gmail.com>
2023-03-17 13:45:25 -0700

    print digits 0 to 9 using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,15 @@
 
 int main(void)
 {
-	int i;
+	int num;
+	char letter;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		putchar(i);
-	}
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
 	putchar('\n');
 	return (0);
 }

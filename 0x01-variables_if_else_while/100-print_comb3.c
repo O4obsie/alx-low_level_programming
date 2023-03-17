0a4c311faffabc9ebe2acea823392c138cca2aec
o4obsie <kodikinya@gmail.com>
2023-03-17 15:02:04 -0700

    print number combiantions

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = '0'; num1 <= 9; num1++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
 		putchar(num1);
-		for (num2 = num1 + 1; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
 			putchar(num2);
 			if (num1 < '9' && num2 < '0')

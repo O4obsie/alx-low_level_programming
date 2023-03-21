5b87d5e58776d642fbc16da085b2f4ae6ac6e99e
o4obsie <kodikinya@gmail.com>
2023-03-21 08:57:29 -0700

    check if letter is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,16 @@
 #include <ctype.h>
 #include "main.h"
 
+/**
+ * _islower - return value if alphabet is lower case
+ *
+ * @c: the input letter
+ *
+ * Return: 1 if lower case, 0 if not
+ */
 int _islower(int c)
 {
-	if (islower(c)>0)
+	if (islower(c) > 0)
 	{
 		return (1);
 	}

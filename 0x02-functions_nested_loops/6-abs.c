3abfeab90694ebf13ca703c2c8726ad16c204a70
o4obsie <kodikinya@gmail.com>
2023-03-21 11:46:50 -0700

    get absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * _abs - gets the absolute value of n
+ *
+ * @n: the input
+ *
+ * Return: absolute nume
+ */
 int _abs(int n)
 {
 	return (abs(n));

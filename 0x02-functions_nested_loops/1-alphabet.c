1c40402c403d7df25d815d647733078eb7c8a324
o4obsie <kodikinya@gmail.com>
2023-03-21 04:18:35 -0700

    print alphabets using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print alphabets
+ */
+void print_alphabet(void)
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z'; lett++)
+	{
+		_putchar(lett);
+	}
+	_putchar('\n');
+}

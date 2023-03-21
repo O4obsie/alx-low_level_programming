fae003031531de7234d3e0c10bdfe6f06ec830b5
o4obsie <kodikinya@gmail.com>
2023-03-21 11:12:33 -0700

    check if nosign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
+/**
+ * print_sign - prints whether the number is positive or negative
+ *
+ * @n: the input number
+ *
+ * Return: 1 if positive, _1 if negative and 0 if 0
+ */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);

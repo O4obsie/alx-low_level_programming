0a0740080d138d4c5deace8d561f1a1788d9ba95
o4obsie <kodikinya@gmail.com>
2023-03-21 09:01:55 -0700

    check if letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,13 @@
 #include <ctype.h>
 #include "main.h"
 
+/**
+ * _isalpha - chec if letter
+ *
+ * @c: letter to check
+ *
+ * Return: 1 if alphabet, 0 if not
+ */
 int _isalpha(int c)
 {
 	if (isalpha(c) != 0)

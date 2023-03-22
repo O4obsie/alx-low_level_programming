e25acaf2a52d08c47a62d6e083f0d17c430d2612
o4obsie <kodikinya@gmail.com>
2023-03-22 13:53:42 -0700

    check sign of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * test - test if number is positive
- *
- * Return: nothing
- */
-void test(int i)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
 {
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }

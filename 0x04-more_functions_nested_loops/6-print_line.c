7161f525a20c1b9646ed98a26d1e3d927b155753
o4obsie <kodikinya@gmail.com>
2023-03-23 06:47:46 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,16 @@ void print_line(int n)
 {
 	int a;
 
-	if (n != 0)
+	if (n > 0)
 	{
 		for (a = 0; a < n; a++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }

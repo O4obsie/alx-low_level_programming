c097895cf323e7f81cd9db80e5c04ef59b1ffcad
o4obsie <kodikinya@gmail.com>
2023-03-23 13:45:44 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
 		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
+			printf("Buzz");
 		else
-		{
-			printf("%i ", i);
-		}
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
-	printf("FizzBuzz\n ");
+	printf("\n");
 	return (0);
 }

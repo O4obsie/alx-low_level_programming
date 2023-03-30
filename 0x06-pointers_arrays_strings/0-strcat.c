1f458825b4ca8fcb279020386ba65e7de93291c1
o4obsie <kodikinya@gmail.com>
2023-03-30 10:06:52 -0700

    concatenate using pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * _strcat - concatenate 2 strings
+ * @dest: string to append to
+ * @src: str we append from
+ * Return: Concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		i++;
+	}
+
+	while (*(src + j == '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	return (dest);
+}

40515c79f6e7324d7787bf16230845923da9b71e
o4obsie <kodikinya@gmail.com>
2023-03-30 10:45:40 -0700

    copy string untill n using pointer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: str to copy to
+ * @src: str to copy from
+ * @n: length to copy
+ * Return: copy of string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while ( i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}

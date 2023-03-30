703b07ca12ffd3de9c2c572d31f3f5b2c64cdd0f
o4obsie <kodikinya@gmail.com>
2023-03-30 12:26:53 -0700

    pointer string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,12 +5,11 @@
  * @s: str input
  * Return: uppercase string
  */
-
 char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (*(s + count) != '\0')
+	while (*(s + i) != '\0')
 	{
 		if ((*(s + i) >= 97) && (*(s + i) <= 122))
 		{

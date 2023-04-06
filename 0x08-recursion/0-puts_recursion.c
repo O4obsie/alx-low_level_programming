b3324608c1e22d971b5395ffd7440022f4bbd859
o4obsie <kodikinya@gmail.com>
2023-04-06 15:52:00 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string
+ *@s: pointer to string
+ */
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		puts('\n');
+		return;
+	}
+
+	puts(*s);
+	_puts_recursion(s + 1);
+
+}

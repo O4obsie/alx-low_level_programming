8e92df03562555c53586b829491a12233ae35c71
o4obsie <kodikinya@gmail.com>
2023-04-17 12:48:41 -0700

    create dog struct && git push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef Struct_DOG
+#define Struct_DOG
+
+/**
+ * struct dog - structure dog
+ * @name: dog anme
+ * @age: dog age
+ * @owner: owner's dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif

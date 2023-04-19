7221c8ac7ae0a100ddde790ba6be3cc5d0039487
o4obsie <kodikinya@gmail.com>
2023-04-19 13:42:33 -0700

    iterrate arr

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * array_iterator - run function on each element of arr
+ * @array: array
+ * @size: size of arr
+ * @action: pointer to function used
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
